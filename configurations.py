bb74d0abec52e759bd0ce81d7af8427dfe475b6d
sylwiaszunejko <sylwia.szunejko@gmail.com>
2025-02-24 12:43:57 +0100

    Remove tests execution with tags cassandra and scylla
    They are no longer used as per https://github.com/scylladb/gocql/pull/399

--- configurations.py
+++ configurations.py
@@ -10,11 +10,9 @@ class TestConfiguration:
 
 
 integration_tests = TestConfiguration(tags=["integration"], test_command_args='-timeout=1m -race -tags="integration"', cluster_configuration={})
-database_tests = TestConfiguration(tags=["scylla", "cassandra"], test_command_args='-timeout=1m -race -tags="scylla cassandra"', cluster_configuration={})
 ccm_tests = TestConfiguration(tags=["ccm"], test_command_args='-timeout=1m -race -tags="ccm"', cluster_configuration={})
 
 test_config_map = {
     "integration": integration_tests,
-    "database": database_tests,
     "ccm": ccm_tests,
 }
